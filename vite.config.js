05ae4a98861a20b735b4c67556067f4d5c1a4188
ipraisethelord <lyndalijun@hotmail.com>
2023-07-09 18:53:19 -0700

    add neceressary files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        404: resolve(__dirname, "public/404.html"),
+        cname: resolve(__dirname, 'public/CNAME'),
+      },
+    },
+  },
+  server: {
+    fs: {
+      // Allow serving robots.txt and sitemap.txt
+      allow: ['.', 'robots.txt', 'sitemap.txt'],
+    },
+  },
+});
