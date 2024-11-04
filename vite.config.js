e8cb6f33ea4bab1a141a529a325bfe1207a1161c
ipraisethelord <127550036+ipraisethelord@users.noreply.github.com>
2024-11-03 21:13:18 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 export default defineConfig({
   plugins: [react()],
-  base: '/thebesthandyman',
+  base: '/thebesthandyman/',
   build: {
     rollupOptions: {
       input: {
